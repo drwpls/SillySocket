04652525a6e822dace3d1623a9f358cd60004885
Doan Kim Huy <doankimhuy.it@gmail.com>
2021-05-01 14:19:09 +0700

    keystroke

--- main/client/client_connect.py
+++ main/client/client_connect.py
@@ -6,28 +6,29 @@ from enum import Enum, auto
 # for debugging
 logging.basicConfig(level=logging.DEBUG)
 
+
 class Client_Connection:
     def __init__(self):
         self.connect_status = self.Status_Code.DISCONNECT
         self.host = '0.0.0.0'
-        self.port = 0 
+        self.port = 0
         self.mainsock = 0
         self.lost_connect = False
 
-    def start_connect(self, host, port, DEBUGGING = False):
+    def start_connect(self, host, port, DEBUGGING=False):
         self.host = host
         self.port = port
         self.mainsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.mainsock.connect((self.host, self.port))
-        except (ConnectionRefusedError, TimeoutError) as e: # this is exception socket.error:
+        except (ConnectionRefusedError, TimeoutError) as e:  # this is exception socket.error:
             logging.debug('Cannot connect to host {}'.format(e))
-            self.connect_status =  self.Status_Code.TIMEOUT   # Timout-status
+            self.connect_status = self.Status_Code.TIMEOUT   # Timout-status
             return
         else:
-            self.connect_status =  self.Status_Code.CONNECTED
             logging.debug('Connected to server {}'.format(self.mainsock.getpeername()))
-        
+            self.connect_status = self.Status_Code.CONNECTED
+
         # begin transfer data: (for commandline debugger)
         if DEBUGGING == True:
             data = '1'
@@ -37,25 +38,22 @@ class Client_Connection:
                 message = data.encode('utf-8')
                 self.mainsock.sendall(message)
         else:
-            # keep thread running
-            data = '1'
-            while not (data == 'Close' or not data) and self.connect_status ==  self.Status_Code.CONNECTED:
-                pass
-    
+            pass
+
     def send_message(self, message):
         message = message.encode('utf-8')
         try:
             self.mainsock.sendall(message)
-        except: # ConnectionAbortedError and ConnectionResetError
+        except:  # ConnectionAbortedError and ConnectionResetError
             logging.debug('Lost connection from {}'.format(self.mainsock.getpeername()))
-            self.connect_status =  self.Status_Code.DISCONNECT
+            self.connect_status = self.Status_Code.DISCONNECT
             self.lost_connect = True
 
     def stop_connect(self):
         logging.debug('Closed connection to {}'.format(self.mainsock.getpeername()))
         endmessage = 'Close'
         self.send_message(endmessage)
-        self.connect_status =  self.Status_Code.DISCONNECT
+        self.connect_status = self.Status_Code.DISCONNECT
 
     class Status_Code(Enum):
         CONNECTED = auto(),
@@ -63,8 +61,9 @@ class Client_Connection:
         CONNECTING = auto(),
         TIMEOUT = auto()
 
+
 if __name__ == '__main__':
     HOST = '192.168.1.11'  # The server's hostname or IP address
     PORT = 65432        # The port used by the server
     client_connection = Client_Connection()
-    client_connection.start_connect(HOST, PORT)
+    client_connection.start_connect(HOST, PORT, True)
---
+++ main/client/keystroke.py
@@ -0,0 +1,71 @@
+import socket
+from PySide6 import QtCore, QtWidgets, QtGui
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+FEATURE_CODE = {
+    'PING': '00',
+    'ProcessRunning': '01',
+    'AppRunning': '02',
+    'Shutdown': '03',
+    'Screenshot': '04',
+    'Keystroke': '05',
+    'RegistryEdit': '06',
+}
+
+
+class Keystroke_Dialog(QtWidgets.QDialog):
+    def __init__(self, sock):
+        super().__init__()
+        self.sock = sock
+        self.setWindowTitle('KeyStroke')
+        self.resize(380, 300)
+    
+        self.Hook = QtWidgets.QPushButton('Hook', self)
+        self.Hook.move(20, 20)
+        self.Hook.setFixedSize(70, 50)
+
+        self.Unhook = QtWidgets.QPushButton('Unhook', self)
+        self.Unhook.move(110, 20)
+        self.Unhook.setFixedSize(70, 50)
+
+        self.Show = QtWidgets.QPushButton('Show keys', self)
+        self.Show.move(200, 20)
+        self.Show.setFixedSize(70, 50)
+
+        self.Clear = QtWidgets.QPushButton('Clear', self)
+        self.Clear.move(290, 20)
+        self.Clear.setFixedSize(70, 50)
+
+        self.Result = QtWidgets.QTextEdit(self)
+        self.Result.move(20, 80)
+        self.Result.setFixedSize(340, 200)
+        self.Result.setReadOnly(True)
+
+        self.Hook.clicked.connect(self.click_hookbutton)
+        self.Unhook.clicked.connect(self.click_unhookbutton)
+        self.Show.clicked.connect(self.click_showbutton)
+        self.Clear.clicked.connect(self.click_clearbutton)
+
+    def click_hookbutton(self):
+        message = FEATURE_CODE['Keystroke'] + 'hook'
+        self.sock.sendall(message.encode(('utf8')))
+
+    def click_unhookbutton(self):
+        message = FEATURE_CODE['Keystroke'] + 'unhook'
+        self.sock.sendall(message.encode(('utf8')))
+
+    def click_showbutton(self):
+        message = FEATURE_CODE['Keystroke'] + 'show'
+        self.sock.sendall(message.encode('utf8'))
+
+        data = self.sock.recv(1024).decode('utf8')
+        print(data)
+        if data == '05forced_exit':
+            pass
+        else:
+            self.Result.setText(data)
+
+    def click_clearbutton(self):
+        self.Result.clear()
